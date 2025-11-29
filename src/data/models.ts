a035cea42fb1d8ae7d8fe57d82d4f09688979475
eduolivag5 <eduolivag5@gmail.com>
2025-11-29 13:51:53 +0100

    disabled gemini model

--- src/data/models.ts
+++ src/data/models.ts
@@ -7,7 +7,7 @@ import { Model } from "../types";
 
 export const models: Model[] = [
     { id: "gpt-4", name: "gpt-4o-mini", company: "OpenAI", icon: "chatgpt-icon.svg", function: getOpenAIResponse, active: true, last_execution_duration: null },
-    { id: "gemini", name: "gemini-1.5-flash", company: "Google", icon: "google-gemini-icon.svg", function: getGeminiResponse, active: true, last_execution_duration: null },
+    { id: "gemini", name: "gemini-1.5-flash", company: "Google", icon: "google-gemini-icon.svg", function: getGeminiResponse, active: false, last_execution_duration: null },
     { id: "deepseek", name: "deepseek-r1", company: "Deepseek", icon: "deepseek-logo-icon.svg", function: getDeepseekResponse, active: false, last_execution_duration: null },
     { id: "mistral-ai", name: "mistral-large-latest", company: "Mistral AI", icon: "mistral-ai-icon.svg", function: getMistralAIResponse, active: true, last_execution_duration: null },
     { id: "grok", name: "grok-2-latest", company: "Twitter", icon: "grok-icon.svg", function: getGROKResponse, active: false, last_execution_duration: null },
